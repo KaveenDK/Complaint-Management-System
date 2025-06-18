48f8a2188fbbeaa05d23abd418c1427665a6a61b
KaveeN <vpdkkaveen@gmail.com>
2025-06-18 11:43:02 +0530

    Initialize Complaint Management System project structure

---
+++ src/main/java/lk/ijse/edu/complaintmanagementsystem/controller/LogoutServlet.java
@@ -0,0 +1,14 @@
+package lk.ijse.edu.complaintmanagementsystem.controller;
+
+/**
+ * --------------------------------------------
+ * @Author Dimantha Kaveen
+ * @GitHub: https://github.com/KaveenDK
+ * --------------------------------------------
+ * @Created 6/15/2025
+ * @Project Complaint Management System - CMS
+ * --------------------------------------------
+ **/
+
+public class LogoutServlet {
+}
---
+++ src/main/java/lk/ijse/edu/complaintmanagementsystem/util/DBUtil.java
@@ -0,0 +1,14 @@
+package lk.ijse.edu.complaintmanagementsystem.util;
+
+/**
+ * --------------------------------------------
+ * @Author Dimantha Kaveen
+ * @GitHub: https://github.com/KaveenDK
+ * --------------------------------------------
+ * @Created 6/15/2025
+ * @Project Complaint Management System - CMS
+ * --------------------------------------------
+ **/
+
+public class DBUtil {
+}
